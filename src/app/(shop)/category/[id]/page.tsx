fd443a6451c00b2d45f2d01d86eee198d4eba62d
Daniel Dìaz <danieldiazc413@gmail.com>
2025-02-09 19:07:19 -0500

    Quantity and Size Components

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,22 +1,37 @@
+import { ProductGrid, Title } from "@/components";
+import { Category } from "@/interfaces";
+import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
 
 interface Props {
   params: {
-    id: string;
+    id: Category;
   };
 }
 
-export default function CategoryIDPage({ params }: Props) {
+const seedProducts = initialData.products;
 
+export default function CategoryIDPage({ params }: Props) {
   const { id } = params;
 
-  if (id === 'kids') {
-    notFound();
+  const products = seedProducts.filter((product) => product.gender === id);
+
+  const labels: Record<Category, string> = {
+    men: 'para hombres',
+    women: 'para mujeres',
+    kid: 'para niños',
+    unisex: 'Para todos',
   }
 
+  // if (id === 'kids') {
+  //   notFound();
+  // }
+
   return (
-    <div>
-      <h1>Category ID Page {id}</h1>
-    </div>
+    <>
+      <Title title={`Articulos ${ labels[id] }`} subtitle="Todos los productos" className="mb-2" />
+
+      <ProductGrid products={ products } />
+    </>
   );
 }
