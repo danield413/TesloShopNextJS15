7fea6df9720c62754cf8a34c47b9f6dd8984bd97
Daniel Dìaz <danieldiazc413@gmail.com>
2025-02-10 15:13:55 -0500

    Completed and enhanced design

--- src/components/index.ts
+++ src/components/index.ts
@@ -8,3 +8,4 @@ export * from './product/size-selector/SizeSelector'
 export * from './product/quantity-selector/QuantitySelector'
 export * from './product/slide-show/SlideShow'
 export * from './product/slide-show/MobileSlideShow'
+export * from './ui/footer/Footer';
